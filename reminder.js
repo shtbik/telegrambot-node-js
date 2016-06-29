c2227faf59d5b3c17782d9684b9c9aec6e6b83cb
Alexander Shtykov <shtykov@live.ru>
2016-06-29 15:21:42 +0300

    Мелкие изменения с минутами

--- reminder.js
+++ reminder.js
@@ -12,12 +12,20 @@
 
 	  notes.push( { 'uid':userId, 'time':time, 'text':text } );
 
+
 	  bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
 	});
 
+	function addZero(i) {
+		if (i < 10) {
+		    i = "0" + i;
+		}
+		return i;
+	}
+
 	setInterval(function(){
 		for (var i = 0; i < notes.length; i++){
-			var curDate = new Date().getHours() + ':' + new Date().getMinutes();
+			var curDate = new Date().getHours() + ':' + addZero(new Date().getMinutes());
 				if ( notes[i]['time'] == curDate ) {
 					bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
 					notes.splice(i,1);
